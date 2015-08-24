6f8f4c41dd3da5f7837c4bee7677f5d78c704f68
Agunq <agunq.e@gmail.com>
2015-08-24 10:52:01 +0700

    Update example.py

--- example.py
+++ example.py
@@ -6,20 +6,6 @@ for anime in data:
   print(anime.title + "\n")
 
 #Example 2
-data = MyAnimeList('one piece', ('user','password')).anime
-for anime in data:
-  anime.moreinfo #to get more info about genres, rating, producers and japanese title
-  print(anime.title)
-  print(anime.genres + "\n")
-
-#Example 3
 data = MyAnimeList('naruto', ('user','password')).manga
 for manga in data:
   print(manga.title + "\n")
-
-#Example 4
-data = MyAnimeList('one piece', ('user','password')).manga
-for manga in data:
-  manga.moreinfo #to get more info about genres, author, serializations and japanese title
-  print(manga.title)
-  print(manga.genres + "\n")
