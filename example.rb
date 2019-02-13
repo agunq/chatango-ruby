462774fedd6f2cc8cb99d39bf1372f1b81b2ede7
Agunq <agunq.e@gmail.com>
2019-02-13 17:25:30 +0700

    Update example.rb

--- example.rb
+++ example.rb
@@ -1,33 +1,37 @@
-require_relative 'ct' #example.rb must in the same path with ct.rb
+require_relative 'ct'
 
-$ws = Chatango.new
+class Bot < Chatango	
 
-def $ws.onConnect(room) 
-    puts "Connected to #{room.name}"
+	def onInitialize
+		self.user.setFontColor "ECE4F1"
+		self.user.setNameColor "FCF"
+		self.enableBg
+	end
+
+	def onPMMessage(pm, user, message)
+		puts "<PM>: <#{user.name}> #{message}"
+		#text = "ya ?"
+		#pm.message user.name, "#{text}"
+	end
+
+	def onConnect(room)
+		puts "Connected to #{room.name}"
+	end
+
+	def onDisconnect(room)
+		puts "Disconnected from #{room.name}"
+	end
+
+	def onMessage(room, user, message)
+		puts "#{room.name}: <#{user.name}> #{message.body}"
+		if message.body == "halo"
+			room.message "halo juga " + user.name
+    
+		end
+    
+	end
+
 end
-def $ws.onDisconnect(room) 
-    puts "Disconnected from #{room.name}"
-end
 
-def $ws.onMessage(room, user, message) 
-    puts "#{room.name}: <#{user.name}> #{message.body}"
-    if message.body == "halo" 
-        room.message "halo juga " + user.name
-    end
-    if message.body == "stop" 
-        room.message "leaving.."
-        $ws.finish
-    end
-    if message.body == "leave" 
-        $ws.leaveRoom("monosekai")
-        room.message "leave room monosekai"
-    end
-    if message.body == "join" 
-        $ws.joinRoom("monosekai")
-        room.message "join room monosekai"
-    end
-end
+Bot.new.start
 
-$ws.start #login as anon
-#login as Anon $ws.start(["nico-nico", "ws4py"])
-#login as User $ws.start(["nico-nico", "ws4py"], "username", "password")
