8a87f74c6a3fa88d8805b9998d90c32510f086ba
Agunq <agunq.e@gmail.com>
2021-05-30 17:58:13 +0700

    Update ct.rb

--- ct.rb
+++ ct.rb
@@ -83,14 +83,14 @@ end
 
 def parseFont(f)
 	if f != nil
-		sizecolor, fontface = f.split("=", 2)
+		sizecolor, fontface = f.split("=", 1)
 		sizecolor = sizecolor.strip()
-		size = sizecolor[1,2].to_i
+		size = sizecolor[1,3].to_i
 		col = sizecolor[3,6]
 		if col == ""
 			col = nil
 		end
-		face = f.split("\"", 3)[1]
+		face = f.split("\"", 2)[1].split("\"", 2)[0]
 		return col, face, size
 	else
 		return nil, nil, nil
@@ -498,6 +498,7 @@ class Room
 		@wlock = false
 		@firstCommand = true
 		@mqueue = {}
+		@umqueue = {}
 		@socket = nil
 		@status = {}
 		@owner = ""
@@ -837,10 +838,39 @@ class Room
 	end
 	
 	def rcmd_mods args
-		mods = args[1..-1].collect{|x| [User(x.split(",")[0]), x.split(",")[1]] }
-		for mod, v in mods
+		new = args[1..-1].collect{|x| [User(x.split(",")[0]), x.split(",")[1]] }
+		old = @mods
+		
+		@mods = {}
+		newmods = []
+		upmods = []
+		delmods = []
+		for mod, v in new
 			@mods[mod] = v
+			if old.include? mod
+				if old[mod] != v
+					upmods << [mod, v, old[mod]]
+				end
+			else
+				newmods <<  [mod, v]
+			end
 		end
+		
+		for mod, v in old
+			if !new.collect{|k,v|k }.include? mod
+				delmods << [mod, v]
+			end
+		end
+		if newmods.size > 0
+			callEvent(:onModAdd, newmods)
+		end
+		if upmods.size > 0
+			callEvent(:onModChange, upmods)
+		end
+		if delmods.size > 0
+			callEvent(:onModRemove, delmods)
+		end
+		
 	end
 	
 	def rcmd_b args
@@ -865,30 +895,49 @@ class Room
 			fontColor, fontFace, fontSize = parseFont(f)
 			mtime = args[1].to_f
 			msg = Message.new(self, user, msg, args[5], args[4], args[7], args[8], mtime, nameColor, fontColor, fontFace, fontSize)
-			msg.attach self, args[6]
+			
+			if msg.user != @mgr.user
+				msg.user.fontColor = msg.fontColor
+				msg.user.fontFace = msg.fontFace
+				msg.user.fontSize = msg.fontSize
+				msg.user.nameColor = msg.nameColor
+			end
+			addHistory msg
+			@channel = msg.badge
+			callEvent(:onMessage, self, msg.user, msg)
 			@mqueue[args[6]]  = msg
+			if @umqueue
+				if @umqueue.keys.include?(args[6])
+					umsgid = @umqueue[args[6]]
+					msg.attach(self, umsgid)
+					@umqueue.delete args[6]
+				end
+			end
+			checkSecret args[6]
 		end
 	end
 
 	def rcmd_u args
+		@umqueue[args[1]]  = args[2]
 		if @mqueue
 			if @mqueue.keys.include?(args[1])
 				msg = @mqueue[args[1]]
 				msg.attach(self, args[2])
-				if msg.user != @mgr.user
-					msg.user.fontColor = msg.fontColor
-					msg.user.fontFace = msg.fontFace
-					msg.user.fontSize = msg.fontSize
-					msg.user.nameColor = msg.nameColor
-				end
-				@mqueue[args[1]] = nil
-				addHistory msg
-				@channel = msg.badge
-				callEvent(:onMessage, self, msg.user, msg)
-			else
-				puts "som secret stuff"
+				@mqueue.delete args[1]
 			end
 		end
+		checkSecret args[1]
+	end
+	
+	def checkSecret check
+		
+		@umqueue.each{|k, v|
+			if k != check 
+				@umqueue.delete(k)
+				@umqueue.each{|ks, vs| puts "some secret #{ks} #{vs} #{self.name}"}
+			end
+		}
+		
 	end
 
 	def rcmd_i args
